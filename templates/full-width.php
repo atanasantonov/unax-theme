c4e59f2d817682f29b5890b47bf9ed6852448a1f
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-06-14 16:39:05 +0300

    Templates added

---
+++ templates/full-width.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * The template displays full-width page without the sidebar
+ *
+ * @package Unax
+ */
+
+get_header();
+
+?>
+	<div id="content" class="site-content <?php echo esc_attr( apply_filters( 'unax_container_fluid_class', 'container-fluid' ) ) ?>">
+
+		<main id="primary" class="site-main">
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+
+	</div><!-- #content -->
+<?php
+
+get_footer();
---
+++ templates/no-sidebar.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: No Sidebar
+ *
+ * The template displays page without the sidebar
+ *
+ * @package Unax
+ */
+
+get_header();
+
+?>
+	<div id="content" class="site-content <?php echo esc_attr( apply_filters( 'unax_container_class', 'container' ) ) ?>">
+
+		<main id="primary" class="site-main">
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+
+	</div><!-- #content -->
+<?php
+
+get_footer();
