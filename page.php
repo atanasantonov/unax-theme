96ab378a0f8aec8dafcbd8a7b13cdd086641e429
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-06-14 15:41:50 +0300

    Indents fix

--- page.php
+++ page.php
@@ -16,27 +16,27 @@ get_header();
 ?>
 	<div id="content" class="site-content <?php echo esc_attr( apply_filters( 'unax_container_class', 'container' ) ) ?>">
 
-	<main id="primary" class="site-main">
+		<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
 
-	</main><!-- #main -->
+		</main><!-- #main -->
 
-	<?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
 
-</div><!-- #content -->
+	</div><!-- #content -->
 <?php
 
 get_footer();
