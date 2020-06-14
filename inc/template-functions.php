5bd8506291309315c9362661cd1099f04d7e8585
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-06-14 14:23:55 +0300

    Sidebar ids chancged

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -5,98 +5,97 @@
  * @package unax
  */
 
-if ( ! function_exists( 'unax_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
+/**
+* Sets up theme defaults and registers support for various WordPress features.
+*
+* Note that this function is hooked into the after_setup_theme hook, which
+* runs before the init hook. The init hook is too late for some features, such
+* as indicating support for post thumbnails.
+*/
+function unax_setup() {
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on unax, use a find and replace
+	 * to change 'unax' to the name of your theme in all the template files.
 	 */
-	function unax_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on unax, use a find and replace
-		 * to change 'unax' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'unax', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'unax', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus(
+		array(
+			'primary-menu' 	=> esc_html__( 'Main menu', 'unax' ),
+			'mobile-menu' 	=> esc_html__( 'Mobile menu', 'unax' ),
+			'footer-menu' 	=> esc_html__( 'Footer menu', 'unax' ),
+		)
+	);
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support(
+		'custom-background',
+		apply_filters(
+			'unax_custom_background_args',
 			array(
-				'primary-menu' 	=> esc_html__( 'Main menu', 'unax' ),
-				'mobile-menu' 	=> esc_html__( 'Mobile menu', 'unax' ),
-				'footer-menu' 	=> esc_html__( 'Footer menu', 'unax' ),
+				'default-color' => 'ffffff',
+				'default-image' => '',
 			)
-		);
+		)
+	);
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'style',
-				'script',
-			)
-		);
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'unax_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
+	/**
+	 * Add support for core custom logo.
+	 *
+	 * @link https://codex.wordpress.org/Theme_Logo
+	 */
+	add_theme_support(
+		'custom-logo',
+		array(
+			'width'       => 200,
+			'height'      => 124,
+			'flex-width'  => true,
+			'flex-height' => true,
+		)
+	);
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'width'       => 200,
-				'height'      => 125,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
-	}
-endif;
+}
 
 
 /**
@@ -120,7 +119,7 @@ function unax_widgets_init() {
 
 	register_sidebar( array(
 		'name'          => esc_html__( 'Header Widget Area', 'unax' ),
-		'id'            => 'sidebar-header',
+		'id'            => 'header-widget-area',
 		'description'   => esc_html__( 'Add widgets here.', 'unax' ),
 		'before_widget' => '',
 		'after_widget'  => '',
@@ -130,7 +129,7 @@ function unax_widgets_init() {
 
 	register_sidebar( array(
 		'name'          => esc_html__( 'Footer Widget Area', 'unax' ),
-		'id'            => 'sidebar-footer',
+		'id'            => 'footer-widget-area',
 		'description'   => esc_html__( 'Add widgets here.', 'unax' ),
 		'before_widget' => '',
 		'after_widget'  => '',
@@ -195,7 +194,7 @@ function unax_grid_columns() {
 	 */
 	$grid_columns = (int)apply_filters( 'unax_grid_columns', $grid_columns_default );
 
-	if( empty( $grid_columns ) || $grid_columns > 6 ) {
+	if( empty( $grid_columns ) || $grid_columns < 1 || $grid_columns > 6 ) {
 		$grid_columns = $grid_columns_default;
 	}
 
