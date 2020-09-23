2b8879d14509a5661a3d9695c9d1455b70c30684
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-09-23 10:28:53 +0300

    Header and content max width fix, entry content common elements margin fix.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -6,40 +6,36 @@
  *
  * @package Unax
  */
-
-$post_content_class = apply_filters( 'unax_post_content_class', 'col-12 col-lg-10 mx-auto' );
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( apply_filters( 'unax_post_single_class', '' ) ); ?>>
 
-	<header class="row entry-header">
+	<header class="entry-header">
 
-		<div class="col-12">
+		<div class="thumbnail">
 			<?php unax_post_thumbnail(); ?>
 		</div>
 
-		<div class="<?php echo esc_attr( $post_content_class ) ?>">
-			<strong class="text-uppercase"><?php the_category( ', ' ); ?></strong>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+
+		<div class="entry-meta">
+			<strong class="entry-category"><?php the_category( ', ' ); ?></strong>
 		</div>
 
 	</header><!-- .entry-header -->
 
-	<div class="row">
-		<div class="entry-content <?php echo esc_attr( $post_content_class ) ?>">
-			<?php
+	<div class="entry-content">
+		<?php
 
-			the_content();
+		the_content();
 
-			wp_link_pages(
-				array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'unax' ),
-					'after'  => '</div>',
-				)
-			);
-			?>
-		</div>
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'unax' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
