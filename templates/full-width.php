7065d63ed8e410406dd1f2fe8207fd30daa63ba1
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-10-19 18:16:47 +0300

    Page full width padding fixes

--- templates/full-width.php
+++ templates/full-width.php
@@ -10,7 +10,7 @@
 get_header();
 
 ?>
-	<div id="content" class="site-content <?php echo esc_attr( apply_filters( 'unax_container_fluid_class', 'container-fluid' ) ) ?>">
+	<div id="content" class="site-content <?php echo esc_attr( apply_filters( 'unax_container_fluid_class', 'container-fluid' ) ); ?>">
 
 		<main id="primary" class="site-main">
 
@@ -18,7 +18,7 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'page-full-width' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
