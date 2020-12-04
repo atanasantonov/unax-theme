297233ac80e047a2732ee600ffbd8e2ca1d0e776
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-12-04 19:28:31 +0200

    Missing filter 'unax_post_class' hook removed from template-filters.php.

--- inc/template-filters.php
+++ inc/template-filters.php
@@ -1,9 +1,7 @@
 <?php
 
+// Body
 add_filter( 'body_class', 'unax_body_classes' );
 
 // Header
 add_filter( 'nav_menu_item_args', 'unax_add_sub_toggles_to_main_menu', 10, 3 );
-
-// Loop
-add_filter( 'unax_post_class', function() { return 'col-lg-6'; } );
