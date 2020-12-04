b4f8cdbce5a7dc17e8b8c23809d159319d0f968d
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-12-04 16:05:57 +0200

    Template functions theme version update, grid columns count fix, FB messanger removed.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -225,10 +225,18 @@ function unax_grid_columns() {
 	/*
 	 * Apply filter to columns count
 	 */
-	$grid_columns = (int) apply_filters( 'unax_grid_columns', $grid_columns_default );
+	$grid_columns = (int) apply_filters( 'unax_grid_columns', $grid_columns );
 
-	if ( empty( $grid_columns ) || $grid_columns < 1 || $grid_columns > 6 ) {
+	if ( empty( $grid_columns ) ) {
 		$grid_columns = $grid_columns_default;
+	}
+
+	if ( $grid_columns < 1 ) {
+		$grid_columns = 1;
+	}
+
+	if ( $grid_columns > 6 ) {
+		$grid_columns = 6;
 	}
 
 	return 'columns-' . $grid_columns;
@@ -268,7 +276,7 @@ function unax_add_sub_toggles_to_main_menu( $args, $item, $depth ) {
 		if ( in_array( 'menu-item-has-children', $item->classes, true ) ) {
 
 			$toggle_target_string = '.menu-modal .menu-item-' . $item->ID . ' > .sub-menu';
-			$toggle_duration      = unax_toggle_duration();
+			$toggle_duration      = apply_filters( 'unax_toggle_duration', 250 );
 
 			// Add the sub menu toggle.
 			$args->after .= '<button class="toggle sub-menu-toggle fill-children-current-color" data-toggle-target="' . $toggle_target_string . '" data-toggle-type="slidetoggle" data-toggle-duration="' . absint( $toggle_duration ) . '" aria-expanded="false"><span class="screen-reader-text">' . __( 'Submenu', 'unax' ) . '</span><i class="fas fa-chevron-down"></i></button>';
@@ -289,58 +297,4 @@ function unax_add_sub_toggles_to_main_menu( $args, $item, $depth ) {
 
 	return $args;
 
-}
-
-
-/**
- * Toggle animation duration in milliseconds.``
- *
- * @return integer Duration in milliseconds
- */
-function unax_toggle_duration() {
-	/**
-	 * Filters the animation duration/speed used usually for submenu toggles.
-	 *
-	 * @since 1.0
-	 *
-	 * @param integer $duration Duration in milliseconds.
-	 */
-	$duration = apply_filters( 'unax_toggle_duration', 250 );
-
-	return $duration;
-}
-
-
-/**
- * Add Facebook Messanger button
- *
- * @return void
- */
-function unax_facebook_messanger() {
-
-	$facebook_page = get_theme_mod( 'facebook_page', '' );
-
-	if ( empty( $facebook_page ) ) {
-		return;
-	}
-
-	?>
-	<a
-		href="<?php echo esc_url( 'https://m.me/' . $facebook_page ); ?>"
-		class="fb-messanger"
-		target="_blank"
-		title="<?php echo esc_attr__( 'Facebook Message', 'unax' ); ?>"
-		>
-		<svg width="60px" height="60px" viewBox="0 0 60 60">
-			<svg x="0" y="0" width="60px" height="60px">
-				<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-					<g>
-						<circle fill="#4267B2" cx="30" cy="30" r="30"></circle>
-						<svg x="10" y="10"><g transform="translate(0.000000, -10.000000)" fill="#FFFFFF"><g id="logo" transform="translate(0.000000, 10.000000)"><path d="M20,0 C31.2666,0 40,8.2528 40,19.4 C40,30.5472 31.2666,38.8 20,38.8 C17.9763,38.8 16.0348,38.5327 14.2106,38.0311 C13.856,37.9335 13.4789,37.9612 13.1424,38.1098 L9.1727,39.8621 C8.1343,40.3205 6.9621,39.5819 6.9273,38.4474 L6.8184,34.8894 C6.805,34.4513 6.6078,34.0414 6.2811,33.7492 C2.3896,30.2691 0,25.2307 0,19.4 C0,8.2528 8.7334,0 20,0 Z M7.99009,25.07344 C7.42629,25.96794 8.52579,26.97594 9.36809,26.33674 L15.67879,21.54734 C16.10569,21.22334 16.69559,21.22164 17.12429,21.54314 L21.79709,25.04774 C23.19919,26.09944 25.20039,25.73014 26.13499,24.24744 L32.00999,14.92654 C32.57369,14.03204 31.47419,13.02404 30.63189,13.66324 L24.32119,18.45264 C23.89429,18.77664 23.30439,18.77834 22.87569,18.45674 L18.20299,14.95224 C16.80079,13.90064 14.79959,14.26984 13.86509,15.75264 L7.99009,25.07344 Z"></path></g></g></svg>
-					</g>
-				</g>
-			</svg>
-		</svg>
-	</a>
-	<?php
 }
