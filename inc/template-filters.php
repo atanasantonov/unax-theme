f5ae3e0d8a0852166d9e6b5101aa821ac18026e0
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-12-04 16:02:27 +0200

    Unax post class filter edited to class with breakpoint 'lg'.

--- inc/template-filters.php
+++ inc/template-filters.php
@@ -5,4 +5,5 @@ add_filter( 'body_class', 'unax_body_classes' );
 // Header
 add_filter( 'nav_menu_item_args', 'unax_add_sub_toggles_to_main_menu', 10, 3 );
 
-add_filter( 'unax_post_class', function() { return 'col-6'; } );
+// Loop
+add_filter( 'unax_post_class', function() { return 'col-lg-6'; } );
