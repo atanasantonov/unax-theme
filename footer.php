6ea1f9bb57d230ba0b9b623af110b4fc4ada92d4
user <29710963+atanasantonov@users.noreply.github.com>
2021-03-21 16:41:02 +0200

    Edit: Footer content

--- footer.php
+++ footer.php
@@ -12,35 +12,57 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info container text-center">
+		<div class="container">
 
-			<?php
-			if ( has_nav_menu( 'footer' ) ) :
-				wp_nav_menu(
-					array(
-						'theme_location'  => 'footer',
-						'menu_class'      => 'footer-menu',
-						'container_class' => 'footer-menu-wrapper',
-						'items_wrap'      => '<ul id="footer-menu-list" class="%2$s">%3$s</ul>',
-						'fallback_cb'     => false,
-					)
-				);
-			endif;
-			?>
+			<div class="row">
+				<div class="col-12 col-lg-8 text-center text-lg-left footer-menu">
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location'  => 'footer',
+								'menu_class'      => 'footer-menu',
+								'container_class' => 'footer-menu-wrapper',
+								'items_wrap'      => '<ul id="footer-menu-list" class="%2$s">%3$s</ul>',
+								'fallback_cb'     => false,
+							)
+						);
+					?>
+				</div>
+				<div class="col-12 col-lg-4 text-center text-lg-right footer-widget-area">
+					<?php dynamic_sidebar( 'footer-widget-area' ); ?>
+				</div>
+			</div><!-- .footer-widget-area -->
 
-			<?php dynamic_sidebar( 'footer-widget-area' ); ?>
-			<br>
-			<?php
-				printf(
-					'<a href="%s" target="_blank">%s</a>',
-					// translators: Footer credits url.
-					esc_url( apply_filters( 'unax_powered_by_url', __( 'https://wordpress.org/', 'unax' ) ) ),
-					// translators: Footer credits text.
-					esc_html( apply_filters( 'unax_powered_by_text', __( 'Proudly powered by WordPress', 'unax' ) ) )
-				);
- 			?>
+			<div class="row site-info">
+				<div class="col-12 col-lg-6 text-center text-lg-left">
+					<?php
+						printf(
+							'<a href="%s" target="_blank">%s</a>',
+							// translators: WordPress credits url.
+							esc_url( apply_filters( 'unax_powered_by_url', __( 'https://wordpress.org', 'unax' ) ) ),
+							// translators: WordPress credits text.
+							esc_html( apply_filters( 'unax_powered_by_text', __( 'Powered by WordPress', 'unax' ) ) )
+						);
+		 			?>
+					|
+					<?php
+						echo esc_html( 'Theme ', 'unax' );
+						printf(
+							'<a href="%s" target="_blank">%s</a>',
+							// translators: Theme credits url.
+							esc_url( apply_filters( 'unax_theme_credits_url', __( 'https://unax.org/unax-theme', 'unax' ) ) ),
+							// translators: Theme credits text.
+							esc_html( apply_filters( 'unax_theme_credits_text', __( 'Unax', 'unax' ) ) )
+						);
+		 			?>
+				</div>
+				<div class="col-12 col-lg-6 text-center text-lg-right">
+					<?php echo esc_html( bloginfo( 'name' ) ); ?>
+				</div>
+			</div><!-- .site-info -->
 
-		</div><!-- .site-info -->
+		</div><!-- .container -->
+
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
