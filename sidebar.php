a85b3d542c3d9618bb05906adba83078950c0818
user <29710963+atanasantonov@users.noreply.github.com>
2022-03-05 11:25:41 +0200

    phpcs

--- sidebar.php
+++ sidebar.php
@@ -11,4 +11,5 @@ if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	<aside id="secondary" class="widget-area">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</aside><!-- #secondary -->
-<?php endif;
+<?php
+endif;
